8b5e5ba305f3b18b03bd5fce6bb8ab1e94941685
dloyolar <dieegolr@gmail.com>
2022-10-18 15:59:40 -0300

    ⚙️ wip(App): add Home and Map Screen

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber Clone!</Text>
-        <StatusBar style="dark" />
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+          <StatusBar style="dark" />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
