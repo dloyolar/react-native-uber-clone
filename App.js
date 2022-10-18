ef0e1b928d9e16eb50d6d4edf5ff220a12ffd317
dloyolar <dieegolr@gmail.com>
2022-10-18 16:14:17 -0300

    ✨ feat(NavOptions): add navigation

--- App.js
+++ App.js
@@ -2,15 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import HomeScreen from './screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
+          </Stack.Navigator>
           <StatusBar style="dark" />
         </SafeAreaProvider>
       </NavigationContainer>
